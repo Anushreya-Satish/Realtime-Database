3aa8757fcd6a3ac6dd303dd3c99ed8b29cab4693
Anushreya Satish <75848533+Anushreya-Satish@users.noreply.github.com>
2022-02-17 20:33:27 +0530

    Add files via upload

---
+++ synchronousBallMovement/sketch.js
@@ -0,0 +1,20 @@
+// declaring
+var canvas, bgImg;
+var gameState = 0;
+var playerCount;
+var database;
+var form, player, game;
+
+function setup(){
+  //create canvas
+  canvas = createCanvas(400,400);
+
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background();
+}
